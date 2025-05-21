a7e6b360af0dfc4c0a689a3134938003eeb590f3
augusto.espindola <augusto.espindola@sesicni.com.br>
2025-05-21 08:53:53 -0300

    Implementação da classe de handler e da doma padrÃo de erro
    	new file:   src/main/java/com/br/oraculo/domain/Erro.java
    	new file:   src/main/java/com/br/oraculo/handler/HandlerExcecoes.java
    	deleted:    src/main/java/com/br/oraculo/handler/HandlerException.java

---
+++ src/main/java/com/br/oraculo/domain/Erro.java
@@ -0,0 +1,33 @@
+package com.br.oraculo.domain;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Erro implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    public Erro(){}
+
+    private String titulo;
+    private String mensagemErro;
+    private Integer statusCode;
+    private String[] erros;
+
+    public Erro(String titulo, String mensagemErro,Integer statusCode){
+        this.titulo = titulo;
+        this.mensagemErro = mensagemErro;
+        this.statusCode = statusCode;
+    }
+
+    public Erro(String titulo, String mensagemErro,Integer statusCode,String[] erros){
+        this.titulo = titulo;
+        this.mensagemErro = mensagemErro;
+        this.statusCode = statusCode;
+        this.erros= erros;
+    }
+}
---
+++ src/main/java/com/br/oraculo/handler/HandlerExcecoes.java
@@ -0,0 +1,23 @@
+package com.br.oraculo.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.br.oraculo.domain.Erro;
+import com.br.oraculo.exception.RecursoNaoEncontradoException;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+public class HandlerExcecoes {
+
+    private static final String VALOR_CONTENTI_TYPE = "application/json;charset=UTF-8";
+    private static final String HEARDER = "Content-Type";
+
+    @ExceptionHandler(RecursoNaoEncontradoException.class)
+    public ResponseEntity<Erro> handlerRecursoNaoEncontrado(RecursoNaoEncontradoException exception){
+        Erro detalheErro = new Erro("Requisição inválida",exception.getLocalizedMessage(),HttpServletResponse.SC_NOT_FOUND);
+        return ResponseEntity.status(detalheErro.getStatusCode()).header(HEARDER,VALOR_CONTENTI_TYPE).body(detalheErro);
+    }
+}
